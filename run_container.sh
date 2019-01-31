0a1124495c1ab74ce336fef8ebff215a3c89b6fb
edjuaro <edjuaro@gmail.com>
2019-01-30 16:52:00 -0800

    using edjuaro/docker-seurat:1.7 now

--- run_container.sh
+++ run_container.sh
@@ -1,1 +1,1 @@
-docker run --rm -v $PWD/Notebooks:/Notebooks -p 8888:8888 -e GRANT_SUDO=yes edjuaro/docker-seurat:1.4 /Notebooks/launch_jupyter.sh
+docker run --rm -v $PWD/Notebooks:/Notebooks -p 8888:8888 -e GRANT_SUDO=yes edjuaro/docker-seurat:1.7 /Notebooks/launch_jupyter.sh
